697a6c56dd974b042c5c980904f2bcef04ba1956
Dylan Robertson <31269647+RobotRage@users.noreply.github.com>
2023-07-10 20:53:31 +1000

    fixed app list refreshing often

---
+++ SolaireProcessManager/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SolaireProcessManager
+{
+    partial class SolaireProcessManager
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.clbProcesses = new System.Windows.Forms.CheckedListBox();
+            this.btnKill = new System.Windows.Forms.Button();
+            this.rtbOutput = new System.Windows.Forms.RichTextBox();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tslRefresh = new System.Windows.Forms.ToolStripLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // clbProcesses
+            // 
+            this.clbProcesses.FormattingEnabled = true;
+            this.clbProcesses.Location = new System.Drawing.Point(303, 80);
+            this.clbProcesses.Name = "clbProcesses";
+            this.clbProcesses.Size = new System.Drawing.Size(233, 334);
+            this.clbProcesses.TabIndex = 1;
+            // 
+            // btnKill
+            // 
+            this.btnKill.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnKill.Location = new System.Drawing.Point(206, 30);
+            this.btnKill.Name = "btnKill";
+            this.btnKill.Size = new System.Drawing.Size(91, 31);
+            this.btnKill.TabIndex = 4;
+            this.btnKill.Text = "Kill Process";
+            this.btnKill.UseVisualStyleBackColor = false;
+            this.btnKill.Click += new System.EventHandler(this.btnKill_Click);
+            // 
+            // rtbOutput
+            // 
+            this.rtbOutput.Location = new System.Drawing.Point(12, 80);
+            this.rtbOutput.Name = "rtbOutput";
+            this.rtbOutput.Size = new System.Drawing.Size(285, 334);
+            this.rtbOutput.TabIndex = 5;
+            this.rtbOutput.Text = "";
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnInfo.Location = new System.Drawing.Point(12, 30);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(91, 31);
+            this.btnInfo.TabIndex = 7;
+            this.btnInfo.Text = "Print Info";
+            this.btnInfo.UseVisualStyleBackColor = false;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Output";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnReset.Location = new System.Drawing.Point(109, 30);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(91, 31);
+            this.btnReset.TabIndex = 10;
+            this.btnReset.Text = "Restart Process";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tslRefresh});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(543, 25);
+            this.toolStrip1.TabIndex = 13;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tslRefresh
+            // 
+            this.tslRefresh.Name = "tslRefresh";
+            this.tslRefresh.Size = new System.Drawing.Size(46, 22);
+            this.tslRefresh.Text = "Refresh";
+            this.tslRefresh.Click += new System.EventHandler(this.tslRefresh_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(300, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Search";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(350, 39);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(186, 20);
+            this.txtSearch.TabIndex = 15;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // SolaireProcessManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(543, 419);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.rtbOutput);
+            this.Controls.Add(this.btnKill);
+            this.Controls.Add(this.clbProcesses);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "SolaireProcessManager";
+            this.Text = "SolaireProcessManager V2";
+            this.Load += new System.EventHandler(this.SolaireProcessManager_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox clbProcesses;
+        private System.Windows.Forms.Button btnKill;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel tslRefresh;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ SolaireProcessManager/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace SolaireProcessManager
+{
+    public partial class SolaireProcessManager : Form
+    {
+        public struct checkboxData
+        {
+            public int PID;
+            public string name;
+            public string cert;
+            public bool selected;
+            public Process proc;
+        }
+
+        checkboxData[] processesStruct;
+
+        Process[] processes = Process.GetProcesses();
+
+        Process[] staticProcesses = Process.GetProcesses();
+
+        string rtbText;
+
+        public SolaireProcessManager()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKill_Click(object sender, EventArgs e)
+        {
+
+            update();
+           // List<int> killed =  new List<int>();
+            for (int i = 0; i < processesStruct.Length; i++)
+            {
+                if (processesStruct[i].selected)
+                {
+                    processesStruct[i].proc.Kill();
+                    timer1.Start();
+                }
+            }
+            
+            reloadList();
+
+            /*
+            rtbText = "";
+
+            bool failed = false;
+            for (int i = 0; i < processesStruct.Length; i++)
+            {
+                if (killed.Contains(processesStruct[i].PID))
+                {
+                    failed = true;
+                    rtbText += "FAILED : Failed to close " + processesStruct[i].name + "\n";
+                }                
+            }
+            if(!failed)
+            {
+                rtbText += "SUCCESS\n";
+            }
+            rtbOutput.Text = rtbText;
+            */
+        }
+
+        int savedScroll = 0;
+
+        void reloadList()
+        {
+            savedScroll = clbProcesses.TopIndex;
+            clbProcesses.Items.Clear();
+            processes = Process.GetProcesses();
+            staticProcesses = Process.GetProcesses();
+            int index = 0;
+            //init struct
+            processesStruct = new checkboxData[processes.Count()];
+
+
+            foreach (Process process in processes.OrderBy(m=>m.ProcessName))
+            {
+                processesStruct[index].PID = process.Id;
+                processesStruct[index].name = process.ProcessName;
+                processesStruct[index].selected = false;
+                processesStruct[index].proc = process;
+
+                clbProcesses.Items.Add(process.ProcessName);
+
+                index++;
+            }
+            
+            clbProcesses.TopIndex = savedScroll;
+        }
+
+        //on start
+        private void SolaireProcessManager_Load(object sender, EventArgs e)
+        {
+            reloadList();
+            timer2.Start();
+        }
+
+        void update()
+        {
+            for (int i = 0; i < processesStruct.Length; i++)
+            {
+                if (clbProcesses.GetItemCheckState(i) == CheckState.Checked)
+                {
+                    processesStruct[i].selected = true;
+                }
+                else
+                {
+                    processesStruct[i].selected = false;
+                }
+            }
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            update();
+            rtbText = "";
+            update();
+            for (int i=0; i < processesStruct.Length; i++)
+            {
+                if (processesStruct[i].selected)
+                {
+                    try
+                    {
+                        rtbText += "Name: " + processesStruct[i].name + " " + "PID: " + processesStruct[i].PID + " PATH: " + processesStruct[i].proc.MainModule.FileName + "\n";
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+            }
+            rtbOutput.Text = rtbText;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            for (int i=0; i < processesStruct.Length; i++)
+            {
+                string templower = processesStruct[i].name.ToLower();
+                if (templower.Contains(txtSearch.Text.ToLower()))
+                {
+                    clbProcesses.TopIndex = i;
+                }
+            }           
+        }
+
+        private void tslRefresh_Click(object sender, EventArgs e)
+        {
+            reloadList();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //reloadList();
+            timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Process[] checkProcess = Process.GetProcesses();
+            //staticProcesses
+
+
+            if (checkProcess.Count() != staticProcesses.Count())
+            {
+                 //reloadList();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            update();
+
+            for (int i = 0; i < processesStruct.Length; i++)
+            {
+                if (processesStruct[i].selected)
+                {
+                    try
+                    {
+                        string path = processesStruct[i].proc.MainModule.FileName;
+                        processesStruct[i].proc.Kill();
+                        Process.Start(path);
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+            }
+
+            reloadList();
+        }
+    }
+}
